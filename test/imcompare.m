2b66f82e0f8f92623c5679acad8cd5a7d786f697
Jean-Francois Pambrun <jf.pambrun@gmail.com>
2015-06-15 11:45:23 -0400

    Return the correct array type with unsigned images.
    Fix #2.

--- test/imcompare.m
+++ test/imcompare.m
@@ -3,10 +3,12 @@ function [] = imcompare(i1name, i2name, size, precision)
   % imcompare('data/000000.raw', '/test/out_000000.raw', [512 512 1], 'int16');
   % imcompare('data/cameraman.10.raw', '/out_cameraman.10.raw', [256 256 1], 'uint8')
   % imcompare('data/saturn.raw', 'out_saturn.raw', [1500 1200 1], 'uint8')
+  % imcompare('data/Uint16FDR_lossless.raw', 'out_Uint16FDR_lossless.raw', [1292 2648 1], 'uint16')
+  
   I1 =multibandread(i1name, size, precision, 0, 'bip', 'ieee-le');
   I2 =multibandread(i2name, size, precision, 0, 'bip', 'ieee-le');
   imshow(abs(I1-I2), [])
-  figure;imshow([I1 I2], [])
+  figure;imshow([I1; I2], [])
   figure;imshow([I2], [])
 
 end
---
+++ test/jp2_U16bit_test.js
@@ -0,0 +1,65 @@
+// load modules
+global.PDFJS = {};
+var fs = require('fs');
+var vm = require('vm');
+vm.runInThisContext(fs.readFileSync('./dist/jpx.js', 'utf8') + '');
+
+function testJp2Decode(filename, test, lossless) {
+    //load jp2 file using
+    var jp2FileAsBuffer = fs.readFileSync('./test/data/' + filename + '.jp2');
+    var jp2FileAsByteArray = new Uint8Array(jp2FileAsBuffer);
+
+    //decode JPEG2000 steam
+    var jpxImage = new global.JpxImage();
+    var startTime = Date.now();
+    jpxImage.parse(jp2FileAsByteArray);
+    var endTime = Date.now();
+    var componentsCount = jpxImage.componentsCount;
+    var tileCount = jpxImage.tiles.length;
+    var tileComponents = jpxImage.tiles[0];
+    var decodedPixelData = tileComponents.items;
+    var height = jpxImage.height;
+    var width = jpxImage.width;
+
+    //load reference raw file
+    var referenceFileAsBuffer = fs.readFileSync('./test/data/' + filename + '.raw');
+
+
+    //compare pixel by pixel
+    var numDiff = 0;
+    var cumDiff = 0;
+    var maxErr = 0;
+    for (var i = 0; i < height * width; i++) {
+        referenceValue = referenceFileAsBuffer.readUInt16LE(i * 2);
+        if (Math.abs(referenceValue - decodedPixelData[i]) > 0) {
+            numDiff++;
+            cumDiff += Math.pow(referenceValue - decodedPixelData[i], 2);
+            if (Math.abs(referenceValue - decodedPixelData[i]) > maxErr) {
+                maxErr = Math.abs(referenceValue - decodedPixelData[i]);
+            }
+        }
+    }
+
+    //write output
+    var buf = new Buffer(height * width * 2);
+    for (var i = 0; i < height * width; i++) {
+        buf.writeUInt16LE(decodedPixelData[i], i * 2);
+    }
+
+    var numSamples = (height * width * componentsCount);
+
+    test.ok((lossless ? maxErr === 0 : maxErr <= 1), numDiff + ' / ' + numSamples + ' degraded pixels, MSE=' + cumDiff / numSamples + ' Max err= ' + maxErr);
+    return {
+        numDiff: numDiff,
+        cumDiff: cumDiff,
+        decodeTime: (endTime - startTime),
+        numSamples: numSamples,
+    }
+}
+
+exports.file_860AE501 = function (test) {
+    debugger;
+    var result = testJp2Decode('860AE501.dcm', test, false)
+        //test.ok(result.decodeTime < 1500, "Decode time is slow (>1500ms)");
+    test.done();
+};
